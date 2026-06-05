cd1468ccb94fd4bf28bb24b6d2700199c1a15334
him-agni <himaniagrawal507@gmail.com>
2026-06-05 09:45:10 -0500

    SarchBar created

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { setQuery } from "../redux/features/searchSlice";
+
+const SearchBar = () => {
+  const [text, setText] = React.useState("");
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(setQuery(text));
+
+    setText("");
+  };
+
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          submitHandler(e);
+        }}
+        className="flex bg-gray-900 gap-5 p-10"
+      >
+        <input
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          required
+          className="w-full border-2 px-4 py-2 text-xl rounded outline-none"
+          type="text"
+          placeholder="Search anything.."
+        />
+        <button className="active:scale-0.95 cursor-pointer border-2 px-4 py-2 text-xl rounded outline-none">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
