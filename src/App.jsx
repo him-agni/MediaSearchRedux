9d0da919f2a9fff1e4559166f94915edfd25ff0e
him-agni <himaniagrawal507@gmail.com>
2026-06-09 14:51:39 -0500

    ResultGrid and ResultCard created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import SearchBar from "./components/SearchBar";
-import Tabs from "./components/Tabs";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CollectionPage from "./pages/CollectionPage";
 
 const App = () => {
   return (
-    <div className="h-screen text-white w-full bg-gray-950">
-      <SearchBar />
-      <Tabs />
+    <div className="text-white w-full bg-gray-950">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/collection" element={<CollectionPage />} />
+      </Routes>
     </div>
   );
 };
